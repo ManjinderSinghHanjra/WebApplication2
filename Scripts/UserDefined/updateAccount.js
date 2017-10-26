cb92bb1b0c8764eda538df255f07ba4fd12f6b3d
ManjinderSinghHanjra <manjindersinghhanjra29@gmail.com>
2017-10-26 19:52:54 +0530

    Unstable.

--- Scripts/UserDefined/updateAccount.js
+++ Scripts/UserDefined/updateAccount.js
@@ -1,5 +1,5 @@
 ﻿var strHostName = "DemoProject";
-var totalButtons = 1;
+var totalButtons = 0;
 var sections;
 var keysEducationRow = ['University', 'Specialization', 'Marks'];
 var keysExperienceRow = ['Organisation', 'Skills', 'Acheivements'];
@@ -16,26 +16,40 @@ function JsonToForm(user) {
             $('form div').find("input" + "[name=" + keys[j] + "]").attr('value', user[keys[j]]);
         }
     }
+
+    if (noOfEducationRows != 0)
+    {
+        $(sections.educationSection).clone().attr('index', j).css({ "display": "inherit" }).insertBefore("#buttonGroup");
+    }
+    if (noOfExperienceRows != 0) {
+        $($(sections.workSection).clone()).css({ "display": "inherit" }).insertBefore("#buttonGroup");
+    }
+    if (noOfResearchRows != 0) {
+        $($(sections.researchSection).clone()).css({ "display": "inherit" }).insertBefore("#buttonGroup");
+    }
+
+
     for (var j = 0; j < noOfEducationRows; j++) {
-        var clone = $($(sections.educationSection).clone()).css({ "display": "inherit" });
+        var clone = $(sections.educationSectionRow).clone().attr('index', j).css({ "display": "inherit" });
         for (var x = 0; x < keysEducationRow.length; x++) {
             $(clone).find('input[name=' + keysEducationRow[x] + ']').val(user['List_Education'][j][keysEducationRow[x]]);
         }
-        $(clone).insertBefore("#buttonGroup");
+        $(clone).appendTo("form #educationSection");
     }
     for (var j = 0; j < noOfExperienceRows; j++) {
-        var clone = $(sections.workSection).clone().css({ "display": "inherit" });
+        var clone = $(sections.workSectionRow).clone().attr('index', j).css({ "display": "inherit" });
         for (var x = 0; x < keysExperienceRow.length; x++) {
             $(clone).find('input[name=' + keysExperienceRow[x] + ']').val(user['List_WorkExperience'][j][keysExperienceRow[x]]);
         }
-        $(clone).insertBefore("#buttonGroup");
+        $(clone).appendTo("form #workSection");
+        totalButtons += 1;
     }
     for (var j = 0; j < noOfResearchRows; j++) {
-        var clone = $($(sections.researchSection).clone()).css({ "display": "inherit" });
+        var clone = $(sections.researchSectionRow).clone().attr('index', j).css({ "display": "inherit" });
         for (var x = 0; x < keysResearchRow.length; x++) {
             $(clone).find('input[name=' + keysResearchRow[x] + ']').val(user['List_ResearchContribution'][j][keysResearchRow[x]]);
         }
-        $(clone).insertBefore("#buttonGroup");
+        $(clone).appendTo("form #researchSection");
     }
 }
 $(document).ready(function () {
@@ -56,23 +70,29 @@ $(document).ready(function () {
 
     sections = {
         educationSection: $("#educationSection").addClass('education'),
+        educationSectionRow: $(".educationSectionRow"),
         workSection: $("#workSection"),
-        researchSection: $("#researchSection")
+        workSectionRow: $(".workSectionRow"),
+        researchSection: $("#researchSection"),
+        researchSectionRow: $(".researchSectionRow")
     }
+
+    // Make all labels colored in white
     $('div > label').css({ 'color': "#fff", 'margin': 'auto' });
 
+    // Click handler on the dropdown in AddNewSection
     $("#addNewSection+ul").on('click', 'li', function () {
         if ($(this).text() == 'Education')
             $(sections.educationSection).css({ "display": "inherit" }).insertBefore("#buttonGroup");
         else if ($(this).text() == 'Work Experience') {
-            $(sections.workSection).find('.btn-danger').attr('disabled', true).end().css("display", "inherit").insertBefore("#buttonGroup");
+            $(sections.workSection).css("display", "inherit").insertBefore("#buttonGroup");
         }
         else if ($(this).text() == 'Research Papers')
             $(sections.researchSection).css("display", "inherit").insertBefore("#buttonGroup");
     });
 
 
-
+    // Click handler on the DeleteSection Button
     $("#deleteSection").on('click', function (e) {
         if ($(this).parent().parent().prev().hasClass('undeleteable')) {
             console.log("Sorry, you can't delete any row beyond this point.");
@@ -81,36 +101,21 @@ $(document).ready(function () {
             $(this).parent().parent().prev().remove();
     });
 
+   
     $("#formDiv").on('click', '#addExtraSkills', function () {
         totalButtons += 1;
-        var parent = $(this).parent().parent();
-        $(parent).find('.btn-danger').attr('disabled', false).end().clone().appendTo("#workSection");
+        var index = $(this).closest('.workSectionRow').attr('index');
+        var clone = $(sections.workSectionRow).clone(true, true).attr('index', parseInt(index)+1).css({ "display": "inherit" });
+        $(clone).appendTo("#workSection");
         $(this).prop('disabled', true);
     });
 
     $("#formDiv").on('click', '#deleteExtraSkills', function () {
-        var prev = $(this).parent().parent().prev();
-        var next = $(this).parent().parent().next();
-        console.log($(prev).attr('id'));
-        console.log($(next).attr('id'));
-        if (totalButtons == 2) {
-            if ($(prev).prev().is('h3')) {
-                $(next).find('.btn-primary').attr('disabled', false);
-                $(next).find('.btn-danger').attr('disabled', true);
-            }
-            else {
-                $(prev).find('.btn-primary').attr('disabled', false);
-                $(prev).find('.btn-danger').attr('disabled', true);
-            }
-            $(this).parent().parent().remove();
+        if (totalButtons <= 2)
+        {
+            $(this).parent().parent().parent().prev().find('.btn-danger').attr('disabled', true);
         }
-        else
-            if ($(prev).attr('id') == 'skillAndMoreRow' && $(next).attr('id') == 'skillAndMoreRow') {
-            }
-            else {
-                $(prev).find('.btn-primary').attr('disabled', false);
-            }
-        $(this).parent().parent().remove();
+        $(this).parent().parent().parent().remove();
         totalButtons -= 1;
     });
 
@@ -125,9 +130,9 @@ $(document).ready(function () {
         if ($('form #educationSection, #workSection, #researchSection').length != 0)
         {
             $('form #educationSection').each(function () {
-                var education = '{';
+                var education = '';
                 $(this).find('input').each(function () {
-                    education += education == '{' ? '' : ',';
+                    education += education == '' ? '' : ',';
                     education +=  '"'+ [$(this).attr('name')] + '":"'  + $(this).val() + '"';
                 });
                 education += '}';
