951effa9a95b7b05762107a348fcddab85029aca
ManjinderSinghHanjra <manjindersinghhanjra29@gmail.com>
2017-10-27 16:47:24 +0530

    Stable
    Changed the logical structure of the UpdateAccount.

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -10,16 +10,13 @@ namespace WebApplication2.Controllers
 {
     public class LoginController : Controller
     {
-
-        /* There is a reason why I have created these constants, one may go for *bool* while writing functions, but give a thorough read to the code and try to understand
-         * the logic. My only motive was to reduce the LOC clutter and these constants have helped me to use switch instead of if-else statements with *boolean* evaluations.
-         */
+        /* Login Request Status Constants */
         const int INITIAL_REQ = -1;         // It's a guest and he's not logged in and was redirected to this page. The guests are not allowed to access the site unless they sign in.
         const int NOTHING_MATCHED = 0;      // Username & Password, both haven't matched
         const int USER_MATCHED = 1;         // Username matched
         const int USER_PASS_MATCHED = 2;    // Username & Password matched!
 
-        // GET: /Login/
+        /* Returns SignUp page view */
         [HttpGet]
         public ActionResult SignUp()
         {
@@ -27,13 +24,15 @@ namespace WebApplication2.Controllers
         }
 
 
+        /* Accepts SignUp form information from the client */
         [HttpPost]
         public ActionResult SignUpSubmitDetails(UserModel oUserModel)
-        {   
+        {
             /* Note:
              *      Don't change the order of the conditions put inside if() clause, we first need to check if elements are null or not null, 
              *      only then we can proceed with the other conditions.
              */
+            /* Checks if the data received is empty or not */
             if (oUserModel == null || oUserModel.EmailID == null || oUserModel.Dob == null || oUserModel.Name == null || oUserModel.Password == null ||
                 oUserModel.EmailID.Equals("") || oUserModel.Dob.Equals("") || oUserModel.Name.Equals("") || oUserModel.Password.Equals("")
                 )
@@ -43,13 +42,15 @@ namespace WebApplication2.Controllers
             List<UserModel> users = (List<UserModel>)HttpContext.Application["users"];
             try
             {
+                /* Whether user already exists or not */
                 switch (oUserModel.Inside(users))
                 {
                     case USER_MATCHED:
                     case USER_PASS_MATCHED:
                         return Json("Sorry, user already exists. If you are an existing user try logging in.");
-                        break; // For safety
-                    default:
+                        break;                // For safety
+
+                    default:                 // Create another user and add it to the Global Users List/Database. And return message SignUp Successful.
                         oUserModel.Id = users.Last().Id + 1;
                         users.Add(oUserModel);
                         HttpContext.Application["users"] = users;
@@ -62,25 +63,34 @@ namespace WebApplication2.Controllers
             }
         }
 
+
+        /* Accepts Login information from the user */
         public ActionResult Login(string email, string password)
         {
+            /* Handle the user depending upon his/her Login Request Status */
             switch (checkCredentials(email, password))
             {
-                case INITIAL_REQ: return View();
-                    break; // For safety
+                case INITIAL_REQ:       // The email & password were null, whatever could be the reasons just show the login page again. 
+                    return View();
+                    break;              // For safety
+
                 case NOTHING_MATCHED:
-                case USER_MATCHED:
+                case USER_MATCHED:      // Return a message to the user that User/Pass mismatched!
                     return Json("Username and/or password mismatch!");
-                    break; // For safety
-                case USER_PASS_MATCHED:
+                    break;              // For safety
+
+                case USER_PASS_MATCHED: // Login credentials matched. Get the user with the email and store it into Session user varible & proceed with ok message!
                     Session["user"] = WebApplication2.Models.Utilities.DataTableUtilities.search(email);
                     ((UserModel)Session["user"]).Type = UserModel.USER;
                     return Json("OK!");
                     break;
+
                 default: return Json("Error Unknown/ Operation Not Supported");
             }
         }
 
+
+        /* Checks if a user emailID & password matches with the database or not. Returns a Login Request Status Constant */
         private int checkCredentials(string emailID, string password)
         {
             UserModel oUserModel = new UserModel();
--- Scripts/UserDefined/updateAccount.js
+++ Scripts/UserDefined/updateAccount.js
@@ -5,34 +5,55 @@ var keysEducationRow = ['University', 'Specialization', 'Marks'];
 var keysExperienceRow = ['Organisation', 'Skills', 'Acheivements'];
 var keysResearchRow = ['Paper', 'Mentions', 'Hyperlink'];
 
+
+/* Fill the User Information Json to this function and it'll dynamically generate all the form & its sections for the UserBoard html page. */
 function JsonToForm(user) {
+    // Getting all the keys inside the User Data will help us to find how many sections are there & is also helping in dynamically matching the User Data Key Names with the Html Form names to fill in the data.
     var keys = Object.keys(user);
     var noOfEducationRows = user['List_Education'] == null ? 0 : user['List_Education'].length;
     var noOfExperienceRows = user['List_WorkExperience'] == null ? 0 : user['List_WorkExperience'].length;
     var noOfResearchRows = user['List_ResearchContribution'] == null ? 0 : user['List_ResearchContribution'].length;
 
+    // Creates the mandatory section of the form. The DOM targeted here is [ form div inputs ]
     for (var j = 0; j < keys.length; j++) {
         if ($('form div').find("input" + "[name=" + keys[j] + "]").length != 0) {
             $('form div').find("input" + "[name=" + keys[j] + "]").attr('value', user[keys[j]]);
         }
     }
 
-    if (noOfEducationRows != 0)
-    {
-        $(sections.educationSection).clone().attr('index', j).css({ "display": "inherit" }).insertBefore("#buttonGroup");
+    // Checks if any section is present in the json data or not. If present then will create the section in the html.
+    // .find('.sectionRow').remove() removes the sectionRow inside it because we want all rows to be filled by using loop constructs and don't want any row existing in prior inside the parent [.section].
+    if (noOfEducationRows != 0) {
+        // Creates #educationSection
+        $(sections.educationSection).clone().find('.sectionRow').remove().end().attr('index', j).css({ "display": "inherit" }).insertBefore("#buttonGroup");
     }
     if (noOfExperienceRows != 0) {
-        $($(sections.workSection).clone()).css({ "display": "inherit" }).insertBefore("#buttonGroup");
+        // Creates #workSection
+        $($(sections.workSection).clone()).find('.sectionRow').remove().end().attr('index', j).css({ "display": "inherit" }).insertBefore("#buttonGroup");
     }
     if (noOfResearchRows != 0) {
-        $($(sections.researchSection).clone()).css({ "display": "inherit" }).insertBefore("#buttonGroup");
+        // Creates #researchSection
+        $($(sections.researchSection).clone()).find('.sectionRow').remove().end().attr('index', j).css({ "display": "inherit" }).insertBefore("#buttonGroup");
     }
 
 
+    // Creates [.educationSectionRow] and appends it to the [form #educationSection]. The new DOM will be [form #educationSection>.educationSectionRow]
+    // If loop ensures if we have to able/disable the buttons in a specific manner within rows which are consecutively being added below others.
+    // .attr(index, j) sets up the index of every single that is being added on the go
     for (var j = 0; j < noOfEducationRows; j++) {
         var clone = $(sections.educationSectionRow).clone().attr('index', j).css({ "display": "inherit" });
         for (var x = 0; x < keysEducationRow.length; x++) {
             $(clone).find('input[name=' + keysEducationRow[x] + ']').val(user['List_Education'][j][keysEducationRow[x]]);
+
+        }
+        if (j == noOfEducationRows - 1) {
+            $(clone).find('.btn-danger').attr('disabled', false);
+            $(clone).find('.btn-primary').attr('disabled', false);
+
+        }
+        else {
+            $(clone).find('.btn-danger').attr('disabled', false);
+            $(clone).find('.btn-primary').attr('disabled', true);
         }
         $(clone).appendTo("form #educationSection");
     }
@@ -40,18 +61,41 @@ function JsonToForm(user) {
         var clone = $(sections.workSectionRow).clone().attr('index', j).css({ "display": "inherit" });
         for (var x = 0; x < keysExperienceRow.length; x++) {
             $(clone).find('input[name=' + keysExperienceRow[x] + ']').val(user['List_WorkExperience'][j][keysExperienceRow[x]]);
+
+        }
+        if (j == noOfExperienceRows - 1) {
+            $(clone).find('.btn-danger').attr('disabled', false);
+            $(clone).find('.btn-primary').attr('disabled', false);
+
+        }
+        else {
+            $(clone).find('.btn-danger').attr('disabled', false);
+            $(clone).find('.btn-primary').attr('disabled', true);
         }
         $(clone).appendTo("form #workSection");
-        totalButtons += 1;
     }
     for (var j = 0; j < noOfResearchRows; j++) {
         var clone = $(sections.researchSectionRow).clone().attr('index', j).css({ "display": "inherit" });
         for (var x = 0; x < keysResearchRow.length; x++) {
             $(clone).find('input[name=' + keysResearchRow[x] + ']').val(user['List_ResearchContribution'][j][keysResearchRow[x]]);
+
+        }
+        if (j == noOfResearchRows - 1) {
+            $(clone).find('.btn-danger').attr('disabled', false);
+            $(clone).find('.btn-primary').attr('disabled', false);
+
+        }
+        else {
+            $(clone).find('.btn-danger').attr('disabled', false);
+            $(clone).find('.btn-primary').attr('disabled', true);
         }
         $(clone).appendTo("form #researchSection");
     }
 }
+
+
+
+// On ready execution
 $(document).ready(function () {
 
     $.ajax({
@@ -68,8 +112,9 @@ $(document).ready(function () {
         }
     });
 
+    // The sections & sectionRows are contained inside sections object, so to make cloning fast and easy.
     sections = {
-        educationSection: $("#educationSection").addClass('education'),
+        educationSection: $("#educationSection"),
         educationSectionRow: $(".educationSectionRow"),
         workSection: $("#workSection"),
         workSectionRow: $(".workSectionRow"),
@@ -81,14 +126,21 @@ $(document).ready(function () {
     $('div > label').css({ 'color': "#fff", 'margin': 'auto' });
 
     // Click handler on the dropdown in AddNewSection
+    // if construct checks if there's already any section present in the page, if no, then creates an entire new [section] and sets up the index of the inner [sectionRow] to be 0
     $("#addNewSection+ul").on('click', 'li', function () {
-        if ($(this).text() == 'Education')
-            $(sections.educationSection).css({ "display": "inherit" }).insertBefore("#buttonGroup");
-        else if ($(this).text() == 'Work Experience') {
-            $(sections.workSection).css("display", "inherit").insertBefore("#buttonGroup");
+        if ($(this).text() == 'Education') {
+            if ($("form").find('#educationSection').length == 0)
+                $(sections.educationSection).css("display", "inherit").find('.sectionRow').css('display', 'inherit').attr('index', '0').end().insertBefore("#buttonGroup");
         }
-        else if ($(this).text() == 'Research Papers')
-            $(sections.researchSection).css("display", "inherit").insertBefore("#buttonGroup");
+
+        else if ($(this).text() == 'Work Experience') {
+            if ($("form").find('#workSection').length == 0)
+                $(sections.workSection).css("display", "inherit").find('.sectionRow').css('display', 'inherit').attr('index', '0').end().insertBefore("#buttonGroup");
+        }
+        else if ($(this).text() == 'Research Papers') {
+            if ($("form").find('#researchSection').length == 0)
+                $(sections.researchSection).css("display", "inherit").find('.sectionRow').css('display', 'inherit').attr('index', '0').end().insertBefore("#buttonGroup");
+        }
     });
 
 
@@ -101,68 +153,83 @@ $(document).ready(function () {
             $(this).parent().parent().prev().remove();
     });
 
-   
-    $("#formDiv").on('click', '#addExtraSkills', function () {
-        totalButtons += 1;
-        var index = $(this).closest('.workSectionRow').attr('index');
-        var clone = $(sections.workSectionRow).clone(true, true).attr('index', parseInt(index)+1).css({ "display": "inherit" });
-        $(clone).appendTo("#workSection");
+
+    // On click handler on AddRowButtons and will add a new [.sectionRow] to the parent [.section]
+    $("#formDiv").on('click', '.addRowButton', function () {
+        closestSectionRow = $(this).closest('.sectionRow');
+        index = $(closestSectionRow).attr('index');
+
+        var clone = $(sections[(closestSectionRow.attr('class').split(' '))[0]]).clone().attr('index', parseInt(index) + 1).css({ "display": "inherit" });
+
+        $(clone).find('.btn-primary').attr('disabled', false).end().appendTo($(closestSectionRow).closest('.section'));
         $(this).prop('disabled', true);
     });
 
-    $("#formDiv").on('click', '#deleteExtraSkills', function () {
-        if (totalButtons <= 2)
-        {
-            $(this).parent().parent().parent().prev().find('.btn-danger').attr('disabled', true);
+    // On click handler on DeleteRowButtons and will delete an existing [.sectionRow] from the parent [.section]
+    // If there exists only one last [.sectionRow] then the entire [.section] is removed
+    $("#formDiv").on('click', '.deleteRowButton', function () {
+        closestSectionRow = $(this).closest('.sectionRow');
+        allSiblingSectionRows = $(closestSectionRow).siblings('.sectionRow');
+        length = allSiblingSectionRows.length + 1;                                      // +1 the element itself : length is the total number of sectionRows inside a Section
+        var index = parseInt(closestSectionRow.attr('index'));
+        if (length == 1) {
+            closestSectionRow.closest('.section').remove();                             //Remove the entire section
         }
-        $(this).parent().parent().parent().remove();
-        totalButtons -= 1;
+        if (parseInt(closestSectionRow.attr('index')) == length - 1) {
+
+            $(allSiblingSectionRows[length - 2]).find('.btn-danger').attr('disabled', false);
+            $(allSiblingSectionRows[length - 2]).find('.btn-primary').attr('disabled', false);
+            closestSectionRow.remove();
+        }
+        else
+            closestSectionRow.remove();
+        $(allSiblingSectionRows).each(function (n) {
+            $(this).attr('index', n);
+        });
+
     });
 
+
+    // Submits the data on the server. All the json data is created using string manipulation.
     $("#updateAccountButton").on('click', function () {
         var user = "";
         var List_Education = "", List_WorkExperience = "", List_ResearchContribution = "";
         $('.mandatory').find('input').each(function () {
-                user += (user == "" ? "" : ",") + '"'+ $(this).attr('name') + '":"' + $(this).val() + '"';
+            user += (user == "" ? "" : ",") + '"' + $(this).attr('name') + '":"' + $(this).val() + '"';
         });
         user += ',';
         console.log(user + "\n");
-        if ($('form #educationSection, #workSection, #researchSection').length != 0)
-        {
-            $('form #educationSection').each(function () {
+        if ($('form #educationSection, #workSection, #researchSection').length != 0) {
+            $('form #educationSection .sectionRow').each(function () {
                 var education = '';
                 $(this).find('input').each(function () {
                     education += education == '' ? '' : ',';
-                    education +=  '"'+ [$(this).attr('name')] + '":"'  + $(this).val() + '"';
+                    education += '"' + [$(this).attr('name')] + '":"' + $(this).val() + '"';
                 });
-                education += '}';
-                List_Education += (List_Education == "" ? "" : ",") +  (education);
+                List_Education += (List_Education == "" ? "" : ",") + '{' + (education) + '}';
             });
-            $('form #workSection').each(function () {
-                var experience = '{';
+            $('form #workSection .sectionRow').each(function () {
+                var experience = '';
                 $(this).find('input').each(function () {
-                    experience += experience == '{' ? '' : ',';
-                    experience +=  '"' + [$(this).attr('name')] + '":"' + $(this).val() + '"';
+                    experience += experience == '' ? '' : ',';
+                    experience += '"' + [$(this).attr('name')] + '":"' + $(this).val() + '"';
                 });
-                experience += '}';
-                List_WorkExperience += (List_WorkExperience == "" ? "" : ",") + (experience);
+                List_WorkExperience += (List_WorkExperience == "" ? "" : ",") + '{' + (experience) + '}';
             });
-            $('form #researchSection').each(function () {
-                var research = '{';
+            $('form #researchSection .sectionRow').each(function () {
+                var research = '';
                 $(this).find('input').each(function () {
-                    research += research == '{' ? '' : ',';
-                    research +=  '"' + [$(this).attr('name')] + '":"' + $(this).val() + '"';
+                    research += research == '' ? '' : ',';
+                    research += '"' + [$(this).attr('name')] + '":"' + $(this).val() + '"';
                 });
-                research += '}';
-                List_ResearchContribution += (List_ResearchContribution == "" ? "" : ",") + (research);
+                List_ResearchContribution += (List_ResearchContribution == "" ? "" : ",") + '{' + (research) + '}';
             });
         }
-        else
-        {
-            // Don't add anything to EducationData;
+        else {
+            // Don't add anything to EducationData or WorkExperience or ResearchContribution. These Sections don't exist at all.
         }
         user += '"List_Education":[' + (List_Education) + '],';
-        user += '"List_WorkExperience":[' + List_WorkExperience +'],';
+        user += '"List_WorkExperience":[' + List_WorkExperience + '],';
         user += '"List_ResearchContribution":[' + List_ResearchContribution + ']';
         $.ajax({
             url: '/' + strHostName + '/Home/UpdateUserCommit',
